9e93237219be596fa082ffb7677c4b15954b7032
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-06-22 12:32:43 -0400

    Set up a basic server using http module

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # node-http
+
+# Part 1:
+set up a basic HTTP server using the core http module, set up a basic request handler and start the server
+
+# Part 2:
+Serve static HTML files with the help of the path and fs core  modules
+
+Learn to use Postman to test the server
+
+# Notes
+npm init command creates package.json file
+
+http.createServer() creates a basic minimal server object using an exisiting http.server class. It takes a request handler callback function as a parameter
+
+Request handler is called everytime the server receives a request. It takes 2 objects as parameters (request and response). We DO NOT create the response object ourselves, we receive it along with the incoming request, then we add data to the response and sent it back. Request and Response objects are special types of objects called streams. 
+
+With streams, data is not transmitted all at once, but in chucks that are read piece by piece.
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const http = require('http');
+//import http module
+const http = require('http')
 
+//declare consts
 const hostname = 'localhost';
 const port = 3000;
 
+//set up server
+//creates a basic minimal server object using an existing http.server class, takes a request handler callback function as a parameter
+//request handler takes 2 objects as parameters (request & response)
 const server = http.createServer((req, res) => {
+    //req object gives us access to headers
     console.log(req.headers);
+
+    //add status code property to the response object, 200 means everything is ok
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello World!</h1></body></html>');
+
+    //set up header for the response object 
+    //tells client what kind of data to expect in the response body, value for this header in second argument
+    res.setHeader('Content-Type', 'text/html')
+
+    //set up response body and close response stream
+    res.end('<html><body><h1>Hello World!</h1></body></html>')
 });
 
+//start server
+//third argument will be callback function that will be executed when the server starts up
 server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}: ${port}/`);
 });

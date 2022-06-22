85b3bf516299d0e9bcf1315bc7d6785c0324d81e
Jessica Del Fierro <jessicabaldric@gmail.com>
2022-06-22 13:13:21 -0400

    serve static HTML files using path and fs modules

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 set up a basic HTTP server using the core http module, set up a basic request handler and start the server
 
 # Part 2:
-Serve static HTML files with the help of the path and fs core  modules
+Serve static HTML files with the help of the path and fs core modules; extend simple http server so that it's capable of serving static files that we have stored in a directory
 
 Learn to use Postman to test the server
 
@@ -16,3 +16,22 @@ http.createServer() creates a basic minimal server object using an exisiting htt
 Request handler is called everytime the server receives a request. It takes 2 objects as parameters (request and response). We DO NOT create the response object ourselves, we receive it along with the incoming request, then we add data to the response and sent it back. Request and Response objects are special types of objects called streams. 
 
 With streams, data is not transmitted all at once, but in chucks that are read piece by piece.
+
+path and fs module are core modules in node -- no need to install anything
+
+if request came in and it was just to the hostname such as localhost, without specifying a url beyond that, req.url property will contain just '/'
+
+path.resolve() method converts from a relative path to an absolute path
+
+fs.access() method lets us know if a file is accessible. Takes 2 arguments, the path that we want to check, then a callback that takes an error argument
+
+res.createReadStream() method takes care of reading the contents of the file that it's given in small chunks rather than all that once so that it doesn't load the whole file into memory, just a chunk at a time (similar to lazy loading in react native); creates a stream object
+
+pipe method to send information from one to the other
+
+# Postman
+used to send test http requests to our server and to view the response from the server.
+
+plus sign to start a new request, it will be set as an http GET request by default
+
+node http server needs to be running
--- server.js
+++ server.js
@@ -5,10 +5,73 @@ const http = require('http')
 const hostname = 'localhost';
 const port = 3000;
 
+//path module and fs module
+const path = require('path');
+const fs = require('fs');
+
 //set up server
 //creates a basic minimal server object using an existing http.server class, takes a request handler callback function as a parameter
 //request handler takes 2 objects as parameters (request & response)
 const server = http.createServer((req, res) => {
+    //console log both url and http method
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    //only want the server to respond to requests with the GET method
+    if (req.method === 'GET') {
+        //examine URL that was requested
+        //local variable that's value is the contents of the request url
+        let fileUrl = req.url;
+
+        //send html.index page automatically if just hostname
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        //get absolute path of file that is requested and store in filePath
+        const filePath = path.resolve('./public' + fileUrl);
+
+        //server only grant requests for html files
+        //create variable and set path.extname method to parse out extension from filePath
+        const fileExt = path.extname(filePath);
+        if (fileExt === '.html') {
+            //check to see if the file exists on the server
+            //takes 2 arguments, path we want to check and a callback that takes an error argument. if file is not accessible then an error object will be passed to err argument
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404; 
+                    res.setHeader('Content-Type', 'text/html'); 
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return; //so code after this is not executed
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+
+                //actually send html file. using pipe method -- sending it over to the response object
+                //piping the data from reading the file located at this file path to the response stream so that the response object can now access that data
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404; 
+            res.setHeader('Content-Type', 'text/html'); 
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not a HTML file</h1></body></html>`);
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html'); //sets content-type to text/html
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
+});
+
+//starts server
+//third argument will be callback function that will be executed when the server starts up
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+/*
+DELETED -- FROM INSIDE REQUEST HANDLER
     //req object gives us access to headers
     console.log(req.headers);
 
@@ -21,10 +84,4 @@ const server = http.createServer((req, res) => {
 
     //set up response body and close response stream
     res.end('<html><body><h1>Hello World!</h1></body></html>')
-});
-
-//start server
-//third argument will be callback function that will be executed when the server starts up
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}: ${port}/`);
-});
+*/
